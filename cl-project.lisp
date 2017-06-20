063b7526054e5c6d0e420542568051708c9c3f29
Thomas de Grivel <thoxdg@gmail.com>
2017-06-20 22:29:06 +0200

    untabify

--- cl-project.lisp
+++ cl-project.lisp
@@ -27,15 +27,15 @@
   (with-output-to-string (out)
     (loop
        (let ((line (read-line stream)))
-	 (when (empty-p line)
-	   (return))
-	 (write-string line out)
-	 (write-char #\Newline out)))))
+         (when (empty-p line)
+           (return))
+         (write-string line out)
+         (write-char #\Newline out)))))
 
 (defmethod header ((pathname pathname))
   (with-open-file (stream pathname
-			  :element-type 'character
-			  :external-format :utf-8)
+                          :element-type 'character
+                          :external-format :utf-8)
     (header stream)))
 
 (defmethod header ((pathname string))
@@ -48,58 +48,58 @@
 
 (defun copy-stream (in out)
   (let ((seq (make-array `(,*buffer-size*)
-			 :element-type 'character)))
+                         :element-type 'character)))
     (loop
        (let ((r (read-sequence seq in)))
-	 (when (= 0 r)
-	   (return))
-	 (write-sequence seq out :end r)))))
+         (when (= 0 r)
+           (return))
+         (write-sequence seq out :end r)))))
 
 (defmethod (setf header) ((value string) (pathname pathname))
   (let ((out-path (pathname (concatenate 'string (namestring pathname)
-					 ".cl-project.tmp"))))
+                                         ".cl-project.tmp"))))
     (with-open-file (in pathname
-			:element-type 'character
-			:external-format :utf-8)
+                        :element-type 'character
+                        :external-format :utf-8)
       (loop
-	 (let ((line (read-line in nil)))
-	   (unless line
-	     (file-position in 0)
-	     (return))
-	   (when (empty-p line)
-	     (return))))
+         (let ((line (read-line in nil)))
+           (unless line
+             (file-position in 0)
+             (return))
+           (when (empty-p line)
+             (return))))
       (with-open-file (out out-path
-			   :direction :output
-			   :external-format :utf-8
-			   :if-does-not-exist :create
-			   :if-exists :supersede)
-	(write-string value out)
-	(write-char #\Newline out)
-	(copy-stream in out)
-	(delete-file in)
-	(rename-file out pathname)
-	value))))
+                           :direction :output
+                           :external-format :utf-8
+                           :if-does-not-exist :create
+                           :if-exists :supersede)
+        (write-string value out)
+        (write-char #\Newline out)
+        (copy-stream in out)
+        (delete-file in)
+        (rename-file out pathname)
+        value))))
 
 (defmethod (setf header) ((value string) (pathname string))
   (setf (header (pathname pathname)) value))
 
 (defun project-asd (dir)
   (directory (make-pathname :name :wild
-			    :type "asd"
-			    :directory (pathname-directory dir)
-			    :defaults dir)))
+                            :type "asd"
+                            :directory (pathname-directory dir)
+                            :defaults dir)))
 
 (defun project-lisp (dir)
   (let* ((wild-dir `(,@(pathname-directory dir) :wild-inferiors))
-	 (wild-pathname (make-pathname :name :wild
-				       :type "lisp"
-				       :directory wild-dir
-				       :defaults dir)))
+         (wild-pathname (make-pathname :name :wild
+                                       :type "lisp"
+                                       :directory wild-dir
+                                       :defaults dir)))
     (directory wild-pathname)))
 
 (defun update-header (project-dir)
   (let* ((asd (first (project-asd project-dir)))
-	 (hdr (header asd)))
+         (hdr (header asd)))
     (format t "~&; ~A~%" (enough-namestring asd))
     (dolist (src (project-lisp project-dir))
       (format t "~&;  ~A~%" (enough-namestring src))
